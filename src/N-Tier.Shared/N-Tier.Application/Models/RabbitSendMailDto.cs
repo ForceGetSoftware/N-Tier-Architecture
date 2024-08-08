43d617b0e22585cf9a55891acc466de9183d9cc9
Necmettin <necmettincimen@forceget.com>
2024-08-08 10:26:34 +0300

    FGS-2662

--- src/N-Tier.Shared/N-Tier.Application/Models/RabbitSendMailDto.cs
+++ src/N-Tier.Shared/N-Tier.Application/Models/RabbitSendMailDto.cs
@@ -6,4 +6,13 @@ public class RabbitSendMailDto
     public string email { get; set; }
     public string cc { get; set; }
     public string data { get; set; }
+    public List<RabbitMqAttachment>? attachment { get; set; }
+}
+
+public class RabbitMqAttachment
+{
+    public string url { get; set; }
+    public string token { get; set; }
+    public string name { get; set; }
+    public string contenttype { get; set; }
 }
