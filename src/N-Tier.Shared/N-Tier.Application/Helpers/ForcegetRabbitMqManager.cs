d6b77f66fb4fb09a149adb51ed8c7fecc9f7afc5
Necmettin <necmettincimen01@gmail.com>
2024-12-01 12:50:01 +0300

    FGS-3489

--- src/N-Tier.Shared/N-Tier.Application/Helpers/ForcegetRabbitMqManager.cs
+++ src/N-Tier.Shared/N-Tier.Application/Helpers/ForcegetRabbitMqManager.cs
@@ -1,3 +1,4 @@
+using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
 using Microsoft.Extensions.Configuration;
@@ -12,32 +13,37 @@ public interface IForcegetRabbitMqManager
     Task<bool> SendMail(RabbitSendMailDto data);
 }
 
-public class ForcegetRabbitMqManager(IConfiguration configuration) : IForcegetRabbitMqManager
+public class ForcegetRabbitMqManager(IConfiguration configuration, IHttpClientFactory clientFactory)
+    : IForcegetRabbitMqManager
 {
     public async Task<bool> Send(string queue, string message)
     {
         var factory = new ConnectionFactory { HostName = configuration["RabbitMq"] };
         await using var connection = await factory.CreateConnectionAsync();
-        await using var channel =await connection.CreateChannelAsync();
-        
+        await using var channel = await connection.CreateChannelAsync();
+
         await channel.QueueDeclareAsync(queue,
             false,
             false,
             false,
             null);
-        
+
         var body = Encoding.UTF8.GetBytes(message);
-        
-        await channel.BasicPublishAsync(string.Empty, queue,body: body);
-        
+
+        await channel.BasicPublishAsync(string.Empty, queue, body: body);
+
         return true;
     }
-    
+
     public async Task<bool> SendMail(RabbitSendMailDto data)
     {
+        var client = clientFactory.CreateClient("NotificationApi");
+        var request = await client.PostAsJsonAsync("/notification/api/Email/Send", data);
+        return request.IsSuccessStatusCode;
+
         var factory = new ConnectionFactory { HostName = configuration["RabbitMq"] };
         await using var connection = await factory.CreateConnectionAsync();
-        await using var channel =await connection.CreateChannelAsync();
+        await using var channel = await connection.CreateChannelAsync();
         var queue = "mail";
         await channel.QueueDeclareAsync(queue,
             false,
@@ -46,10 +52,10 @@ public class ForcegetRabbitMqManager(IConfiguration configuration) : IForcegetRa
             null);
 
         var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(data));
-        
-        await channel.BasicPublishAsync(string.Empty, queue,body: body);
 
-        
+        await channel.BasicPublishAsync(string.Empty, queue, body: body);
+
+
         return true;
     }
 }
