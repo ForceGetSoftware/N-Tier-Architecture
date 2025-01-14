785bf37653cd8315c89d5f0273ba46812fff0bc4
Necmettin <necmettincimen01@gmail.com>
2025-01-14 08:45:20 +0300

    FGS-3489

--- src/N-Tier.Shared/N-Tier.DataAccess/Persistence/ForcegetDatabaseContext.cs
+++ src/N-Tier.Shared/N-Tier.DataAccess/Persistence/ForcegetDatabaseContext.cs
@@ -8,7 +8,6 @@ namespace N_Tier.DataAccess.Persistence;
 
 public class ForcegetDatabaseContext(DbContextOptions options, IClaimService claimService) : DbContext(options)
 {
-    public DbSet<N8nWorkflows> N8nWorkflows { get; set; }
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
--- src/N-Tier.Shared/N-Tier.DataAccess/Repositories/Impl/N8nWorkflowsRepository.cs
+++ src/N-Tier.Shared/N-Tier.DataAccess/Repositories/Impl/N8nWorkflowsRepository.cs
@@ -1,10 +1,11 @@
 ﻿using Auth.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 using N_Tier.DataAccess.Persistence;
 using N_Tier.Shared.N_Tier.DataAccess.Repositories;
 
 namespace N_Tier.DataAccess.Repositories.Impl;
 
-public class N8nWorkflowsRepository(ForcegetDatabaseContext context, IBaseMongoRepository mongoRepository)
+public class N8nWorkflowsRepository(DbContext context, IBaseMongoRepository mongoRepository)
     : BaseRepository<N8nWorkflows>(context, mongoRepository), IN8nWorkflowsRepository
 {
 }
